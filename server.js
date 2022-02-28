dbce4c16df803f7136d963f2d0f42b496a1ecf08
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-28 15:52:40 -0800

    Added fallback routes and mongoDB connection

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 'use strict';
 
+// Assigning packages with require
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+
+// Middle ware
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+// const Book = require('./models/book.js');
+
+mongoose.connect(process.env.DB_URL);
+
+// Test of life codeblock for checking our DB connection is good.
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+// ----ROUTES----
 app.get('/test', (request, response) => {
 
   response.send('test request received');
 
 });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.status(200).send('The API is happy and alive.');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('You goofed.');
+});
+
+// Express error catcher
+app.use((error, req, res, next) => { // eslint-disable-line
+  res.status(error.status).send('You goofed.');
+});
+
+app.listen(PORT, () => console.log('listening on port: ',PORT));
