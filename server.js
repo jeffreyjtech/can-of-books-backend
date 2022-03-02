083e75bd00c29ed4a1e9acc125eb1937bc514ac2
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-01 17:14:48 -0800

    Added json middleware and postBook handler

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
 // Require-in our Book model
@@ -28,6 +30,8 @@ db.once('open', function () {
 // ----ROUTES----
 app.get('/books', getBooks);
 
+app.post('/books', postBook);
+
 async function getBooks(req, res, next){
   try {
     // let queryObject = {email: req.query.email || 'j-d-salinger@email.scam'};
@@ -35,6 +39,18 @@ async function getBooks(req, res, next){
     // Here we're using our Book model for a mongoose query (.find())
     let results = await Book.find();
     res.status(200).send(results);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function postBook(req, res, next){
+  try{
+    // console.log(req.body);
+
+    let createdBook = await Book.create(req.body);
+
+    res.status(200).send(createdBook);
   } catch (error) {
     next(error);
   }
